d330b85bce176d16f1fbce794255c92079fd9268
Mark <crayvolt@gmail.com>
2013-01-31 18:56:52 +0800

    Create a rail web app plus status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crayvolt::Application.config.secret_token = 'e5b66feb961f7a8d0c733af62b5558d7d1d1180a798e9df061f2ea80135cd99faab66c9e980363ca9278f67125b1ef7bf951c6b47f750a16c8ec473465146d2f'
